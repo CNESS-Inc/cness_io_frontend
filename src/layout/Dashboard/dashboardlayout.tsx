9def52964fe7701791e7ecd0eb702f042e58f183
Nandy-5 <nandy.it5@gmail.com>
2025-05-15 00:13:06 +0530

    score and results page

--- src/layout/Dashboard/dashboardlayout.tsx
+++ src/layout/Dashboard/dashboardlayout.tsx
@@ -4,36 +4,37 @@ import DashboardHeader from './DashboardHeader';
 import DashboardNavbar from './DashboardNavbar';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
-  const toggleMobileNav = () => setIsMobileNavOpen(!isMobileNavOpen);
+   const toggleMobileNav = () => setIsMobileNavOpen(!isMobileNavOpen);
 
-  return (
-    <div className="bg-[#f9f9f9] flex flex-row justify-center w-full min-h-screen">
-      <div className="bg-[#f9f9f9] w-full max-w-[1440px] flex flex-col">
-        {/* Mobile Header */}
-        {!isMobileNavOpen && (
-          <div className="md:hidden">
-            <DashboardHeader toggleMobileNav={toggleMobileNav} />
-          </div>
-        )}
+ return (
+ <div className="bg-[#f9f9f9] flex flex-row justify-center w-full min-h-screen">
+ <div className="bg-[#f9f9f9] w-full max-w-[1440px] flex flex-col">
+ {/* Mobile Header */}
+{!isMobileNavOpen && (
+<div className="md:hidden">
+ <DashboardHeader toggleMobileNav={toggleMobileNav} />
+</div>
+)}
 
-        <div className="flex flex-col md:flex-row">
-          <DashboardNavbar isMobileNavOpen={isMobileNavOpen} toggleMobileNav={toggleMobileNav} />
+ <div className="flex flex-col md:flex-row">
+<DashboardNavbar isMobileNavOpen={isMobileNavOpen} toggleMobileNav={toggleMobileNav} />
 
-          {/* Main content */}
-          <div className="flex-1 flex flex-col">
-            {/* Desktop Header */}
-            <div className="hidden md:block">
-              <DashboardHeader />
-            </div>
+ {/* Main content */}
+<div className="flex-1 flex flex-col">
+{/* Desktop Header */}
+<div className="hidden md:block">
+<DashboardHeader />
+</div>
 
-            <main>{children}</main>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+<main>{children}</main>
+</div>
+</div>
+</div>
+</div>
+ );
 };
 
 export default DashboardLayout;
+
