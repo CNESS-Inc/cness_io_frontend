5676d79cf5378eaa4a9363c2c239f2fd0584ab53
fullstackdev1008 <fullstackdev1008@gmail.com>
2025-05-26 21:59:48 +0530

    live api change

--- src/Common/ServerAPI.tsx
+++ src/Common/ServerAPI.tsx
@@ -56,8 +56,8 @@ export const ServerAPI = {
 
 export const API = {
   // BaseUrl: "http://192.168.1.3:5025/api", //local
-  BaseUrl: "http://localhost:5025/api", //local
-  // BaseUrl: "https://z3z1ppsdij.execute-api.us-east-1.amazonaws.com/api", //live
+  // BaseUrl: "http://localhost:5025/api", //local
+  BaseUrl: "https://z3z1ppsdij.execute-api.us-east-1.amazonaws.com/api", //live
 };
 
 export const EndPoint = {
