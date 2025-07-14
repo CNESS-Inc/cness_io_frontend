283d2ccf5f9d6db01ccd430c3596f49351b524a4
fullstackdev1008 <fullstackdev1008@gmail.com>
2025-07-14 22:07:48 +0530

    Update ServerAPI.tsx

--- src/Common/ServerAPI.tsx
+++ src/Common/ServerAPI.tsx
@@ -719,7 +719,7 @@ export const executeAPI = async <T = any,>(
           : { "Content-Type": "application/json" }),
         Authorization: `Bearer ${token || ""}`,
       },
-      ...(API.BaseUrl.trim().toLowerCase().startsWith("https://") && { withCredentials: false })
+      ...(API.BaseUrl.trim().toLowerCase().startsWith("https://") && { withCredentials: true })
     });
 
     const access_token = response.headers['access_token'];
