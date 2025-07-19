d0f88fe0c484f41379acd8683544dfe07a1929db
cnessviji <viji@cness.co>
2025-07-18 22:00:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
-//dummy commit to trigger the deployment
+
 function App() {
   return <RouterProvider router={router} />
 }
