81a18f048389c61c2ab30cb023b6363afdf51ecb
developercmit <developersuseonly@gmail.com>
2025-07-25 17:46:43 +0530

    resolved affiliate and image/video validation

--- src/Common/ServerAPI.tsx
+++ src/Common/ServerAPI.tsx
@@ -72,8 +72,8 @@ export const ServerAPI = {
 
 export const API = {
   //  BaseUrl: "http://192.168.1.30:5025/api", //local
-  BaseUrl: "http://localhost:3000/api", //local
-  // BaseUrl: import.meta.env.VITE_API_BASE_URL || "https://z3z1ppsdij.execute-api.us-east-1.amazonaws.com/api",
+  // BaseUrl: "http://localhost:3000/api", //local
+  BaseUrl: import.meta.env.VITE_API_BASE_URL || "https://z3z1ppsdij.execute-api.us-east-1.amazonaws.com/api",
 };
 
 export const EndPoint = {
