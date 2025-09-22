78f12bde8f641f3deeb200867d213060a644e5b1
jasmin-malaviya <jasmin@cness.co>
2025-09-22 17:24:08 +0530

    build error solve

--- src/components/ui/ResetPassword.tsx
+++ src/components/ui/ResetPassword.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent, type FormEvent } from "react";
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import { ForgotPasswordDetailsSubmit } from "../../Common/ServerAPI";
